84951aa4f7cea8d6edf4c74553fd8ea4f6a4499e
seattlenomnom <mark.crapser@gmail.com>
2022-07-12 18:57:13 -0700

    filled in main()

--- catchararray.c
+++ catchararray.c
@@ -10,23 +10,17 @@ void concat(char result[], const char str1[], int n1,
 
 int main(int argc, char *argv[]) {
 
+    const char s1[5] = {'T', 'e', 's', 't', ' '};
+    const char s2[6] = {'w', 'o', 'r', 'k', 's', '.'};
+    char s3[11];
+    int i;
 
+    concat(s3, s1, 5, s2, 6);
 
+    for(i = 0; i < 11; ++i)
+        printf("%c", s3[i]);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    printf("\n");
 
     return(0);
 }
